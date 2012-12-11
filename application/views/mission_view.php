e87270d6a112a0c926444b1c8d1726e2dd65bf0f
Roger Qiu <roger.qiu@polycademy.com>
2012-12-12 09:14:41 +1100

    Designed it better

--- application/views/mission_view.php
+++ application/views/mission_view.php
@@ -2,34 +2,39 @@
 		<div class="container">
 			<? if(!empty($mission_data)){ ?>
 				<article class="mission_testing">
+				
 					<h1><?=$mission_data['title']?></h1>
-					<div class="mission_grid row-fluid">
-						<div class="span4 mission_parameters">
-							<section class="parameter_container">
-								<h4>Sitrep</h4>
-								<div class="mission_description">
-									<?=$mission_data['description']?>
+					
+					<div class="mission_grid row">
+					
+						<section class="mission_sitrep span6">
+							<div class="mission_description">
+								<?=$mission_data['description']?>
+							</div>
+						</section>
+						
+						<aside class="mission_execution span6">
+							<div class="execution_container span6" data-spy="affix" data-offset-top="70">
+								<div class="editor_container">
+									<?= form_open($code_submit, array('class'=>'mission_code_form')) ?>
+										<?= form_textarea(
+											array(
+												'name'			=> 'code',
+												'id'			=> 'codemirror',
+												'value'			=> $mission_data['default'],
+											)
+										) ?>
+										<?= form_submit(array('name'=>'submit', 'type'=>'submit', 'value'=>'Execute!', 'class'=>'btn btn-primary code_submit')) ?>
+									<?= form_close() ?>
 								</div>
-							</section>
-						</div>
-						<div class="span8 mission_execution">
-							<section class="editor_container">
-								<?= form_open($code_submit, array('class'=>'mission_code_form')) ?>
-									<?= form_textarea(
-										array(
-											'name'			=> 'code',
-											'id'			=> 'codemirror',
-											'value'			=> $mission_data['default'],
-										)
-									) ?>
-									<?= form_submit(array('name'=>'submit', 'type'=>'submit', 'value'=>'Execute!', 'class'=>'btn btn-primary code_submit')) ?>
-								<?= form_close() ?>
-							</section>
-							<section class="output_container">
-								<div></div>
-							</section>
-						</div>
+								<div class="output_container">
+									<div></div>
+								</div>
+							</div>
+						</aside>
+					
 					</div>
+					
 				</article>
 				<script src="<?= base_url($js_assets) ?>/codemirror/codemirror.js"></script>
 				<script src="<?= base_url($js_assets) ?>/codemirror/mode/php/php.js"></script>
